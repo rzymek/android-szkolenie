05b5f3681d7484827380cd98dbe63f26cadf15ac
rzymek <rzymek@gmail.com>
2014-05-06 11:33:49 +0200

    tryToLogin

--- src/org/rzymek/todoexpert/LoginActivity.java
+++ src/org/rzymek/todoexpert/LoginActivity.java
@@ -1,27 +1,61 @@
 package org.rzymek.todoexpert;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
+
+	private EditText userEdit;
+	private EditText passEdit;
+	private Button doLogin;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+
+		userEdit = (EditText) findViewById(R.id.user);
+		passEdit = (EditText) findViewById(R.id.pass);
+		doLogin = (Button) findViewById(R.id.doLogin);
+
+		doLogin.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				tryToLogin();
+			}
+		});
+	}
+
+	protected void tryToLogin() {
+		String user = userEdit.getText().toString();
+		String pass = passEdit.getText().toString();
+		boolean failed = false;
+		if (TextUtils.isEmpty(user)) {
+			userEdit.setError(getString(R.string.empty_value));
+			failed = true;
+		}
+		if (TextUtils.isEmpty(pass)) {
+			passEdit.setError(getString(R.string.empty_value));
+			failed = true;
+		}
+		if (failed)
+			return;
+		login(user,pass);
+	}
+
+	private void login(String user, String pass) {
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
