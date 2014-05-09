512bc7cda9885467936fe917eb14adccddce2a34
rzymek <rzymek@gmail.com>
2014-05-09 15:58:17 +0200

    autocomplete ok

--- src/rzymek/sandbox/MainActivity.java
+++ src/rzymek/sandbox/MainActivity.java
@@ -1,7 +1,10 @@
 package rzymek.sandbox;
 
+import java.util.Arrays;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
@@ -17,7 +20,9 @@ public class MainActivity extends Activity {
 		auto = (AutoCompleteTextView) findViewById(R.id.auto);
 		ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
 		adapter.clear();
-		adapter.addAll("Object Relational Mapping Lite (ORM Lite) provides some lightweight functionality for persisting Java objects to SQL databases while avoiding the complexity and overhead of more standard ORM packages. It supports a number of SQL databases using JDBC and also supports Sqlite with native calls to Android OS database APIs. Documentation about how to configure ORMLite for Android specifically is available in the manual.".split(" *"));
+		String[] split = "Object Relational Mapping Lite (ORM Lite) provides some lightweight functionality for persisting Java objects to SQL databases while avoiding the complexity and overhead of more standard ORM packages. It supports a number of SQL databases using JDBC and also supports Sqlite with native calls to Android OS database APIs. Documentation about how to configure ORMLite for Android specifically is available in the manual.".split(" ");
+		Log.w("XXX",Arrays.asList(split).toString());
+		adapter.addAll(Arrays.asList(split));
 		auto.setAdapter(adapter);
 	}
 }
